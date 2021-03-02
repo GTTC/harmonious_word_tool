9e4a72b1b673e1ce2285c51b3ebc55461dff4ea0
Gallium Wang <galliumwang199@gmail.com>
2021-03-03 02:22:53 +0800

    file upload example

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,32 @@
 var http = require('http');
+var formidable = require('formidable');
+var fs = require('fs');
+const mv = require('mv');
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
+  if (req.url == '/fileupload') {   //file upload url
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+      // console.log(req); //FIXME
+      var oldpath = files.filetoupload.path;
+      var newpath = './uploads/' + files.filetoupload.name;
+
+      mv(oldpath, newpath, function (err) {
+        if (err) {
+            console.log('> FileServer.jsx | route: "/files/upload" | err:', err);
+            throw err;
+        }
+        res.write('File uploaded and moved!');
+        res.end();
+    });
+
+ });
+  } else {  //file upload form
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+    res.write('<input type="file" name="filetoupload"><br>');
+    res.write('<input type="submit">');
+    res.write('</form>');
+    return res.end();
+  }
 }).listen(8080);

05ef0893e836bfaa43984686a87c15d45b25ea22
Gallium Wang <galliumwang199@gmail.com>
2021-03-05 18:49:04 +0800

    update UI and complete the function

--- frontend/script.js
+++ frontend/script.js
@@ -7,12 +7,22 @@ function onload() {
     auto_grow(document.getElementById("result"));
 }
 
+function insert_char(raw_str,inserted_char){
+    return raw_str.split('').join(inserted_char);
+}
 
-function generate() {
-    let default_char = document.getElementById("default-char").value;
+function generate_default() {
+    let origin_value = document.getElementById("result").value
+    let default_char = document.getElementById("default-char").value[1];
+    let result=insert_char(origin_value,default_char);
+    document.getElementById("result").value = result;
+}
+
+
+function generate_custom() {
+    let origin_value = document.getElementById("result").value
     let custom_char=document.getElementById("custom-char").value;
 
-
-
+    let result=insert_char(origin_value,custom_char);
     document.getElementById("result").value = result;
 }
